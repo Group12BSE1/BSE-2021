06da0af24a6690b6c8b8fb7b0ed1b9ac7a7d2918
GROUP12BSE1 <group12bse1@outlook.com>
2021-05-15 09:55:19 +0300

    mid sem

--- src/chapter 5/exercisE 3.py
+++ src/chapter 5/exercisE 3.py
@@ -1,98 +1,53 @@
-nickel = 25
-dime = 25
-quarter = 0
-one = 0
-five = 0
-print(nickel, "nickels")
-print(dime, "dimes")
-print(quarter, "quarters")
-print(one, "ones")
-print(five, "fives")
-#Repeatedly asking for the price
-while True:
-    price = input("Enter the purchase price (xx.xx) or 'q' to quit:")
-if price == 'q':
-        quit()
-else:
-    #checking the user's input
-    price = float(price)
-    amount = price * 100
-    #for multiple remainder = 0
-    remainder = amount % 5
-    if remainder == 0 and price > 0:
-        print("Menu for deposits:\n")
-        print(" 'n' - deposit a dime")
-        print(' "d" - deposit a dime ')
-        print(' "q" - deposit a quarter ')
-        print(' "o" - deposit a one dollar bill')
-        print(' "f" - deposit a five dollar bill')
-        print(' "c" - cancel the purchase')
-        dollars = amount // 100
-        cent = amount % 100
-        print("Amount due:",dollars,'dollars and', cent-s,'cents')
-        payment = 0
-        #Getting the user's selection
-        while True:
-                deposit = input("Indicate your selection:")
-                if deposit == 'n':
-                    while True:
-                        payment = payment + 5
-                        amount_due = amount - payment
-                        break
-                    if payment < amount:
-                        print(amount_due//100, 'dollars and' , amount_due % 100, 'cents')
-                    continue
-                else:
-                    print("Take change")
-                    break
-            if deposit == 'd':
-                while True:
-                    payment = payment + 10
-                    amount_due = amount - payment
-                    break
-                if payment < amount:
-                    print(amount_due//100,'dollars and' , amount_due % 100,'cents')
-                continue
-            else:
-                 break
-            if deposit == 'q':
-                while True:
-                    payment = payment + 25
-                    amount_due = amount - payment
-                    break
-                if payment < amount :
-                    print(amount_due//100, 'dollars and' ,amount_due % 100,'cents')
-                continue
-             else:
-                print("Take the change")
-            if deposit == 'o':
-                while True:
-                    payment = payment + 100
-                    amount_due = amount - payment
-                    break
-                if payment < amount:
-                    print(amount_due//100, 'dollars and',amount_due % 100, 'cents')
-                continue
-            else:
-                print("Take change")
-                break
-            if deposit =='f':
-                while True:
-                    payment = payment + 500
-                    amount_due = amount - payment
-                    break
-                if payment < amount:
-                    print(amount_due//100,'dollars and',amount_due % 100,'cents')
-                continue
-            else:
-                print("Take change")
-                break
-            if deposit == 'c':
-                if payment > 0:
-                    print("Please take the change below")
-                    break
-                else:
-                    break
+nickels = 25
+dimes = 25
+quarters = 25
+one_dollar = 0
+five_dollar = 0
+
+
+def menu_message():
+    print('MENU FOR DEPOSITS')
+    print('n -- deposit a nickel')
+    print('d -- deposit a dime')
+    print('q -- deposit a quarter')
+    print('o -- deposit a one dollar bill')
+    print('f -- deposit a five dollar bill')
+    print('c -- caancel the deposit')
+
+
+def stock_contains(n_value, d_value, q_value, o_value, f_value):
+    print(f'{n_value} nickels')
+    print(f'{d_value} dimes')
+    print(f'{q_value} quarter')
+    print(f'{o_value} one dollar bill')
+    print(f'{f_value} five dollar bill')
+
+
+def testUserInput(value):
+    status = ''
+    dollar = value // 1
+    cents = value % 1
+    finalCents = round(cents, 2)
+    centsToFull_digit = finalCents * 100
+    totalCents = (dollar * 100) + round(centsToFull_digit)
+    if (totalCents % 5) == 0 and totalCents >= 0:
+        status = 'YES'
     else:
-        print("Illegal selection: Must be a non negative multiple of 5")
-continue
+        status = 'NO'
+    return status
+
+
+print("Welcome to the vending machine change maker program \n Change maker initialized")
+stock_contains(nickels, dimes, quarters, one_dollar, five_dollar)
+AmountToPay = float(input("Enter the purchase price (xx.xx) or 'q' to quit: "))
+status = testUserInput(AmountToPay)
+if status == "YES":
+
+
+
+
+
+
+
+
+
